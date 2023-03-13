5cda032960ab5774647dba3468dd053a68473c91
Cian Johnston <public@cianjohnston.ie>
2023-03-13 11:14:07 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,4 +8,4 @@ ssh-add -l || true
 
 ssh -T git@github.com || true
 
-SERVICE_URL=https://open-vsx.org/vscode/gallery ITEM_URL=https://open-vsx.org/vscode/item /var/tmp/coder/code-server/bin/code-server --install-extension ms-python.python
+SERVICE_URL=https://open-vsx.org/vscode/gallery ITEM_URL=https://open-vsx.org/vscode/item /var/tmp/coder/code-server/bin/code-server --install-extension ms-python.python --force
