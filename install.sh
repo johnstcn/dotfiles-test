ed3dde1279408f375b67eecdad4fadcef30ae279
Cian Johnston <public@cianjohnston.ie>
2024-06-25 13:44:19 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,10 +2,4 @@
 
 set -x
 
-env
-
-ssh-add -l || true
-
-ssh -T git@github.com || true
-
-SERVICE_URL=https://open-vsx.org/vscode/gallery ITEM_URL=https://open-vsx.org/vscode/item /var/tmp/coder/code-server/bin/code-server --install-extension ms-python.python --force
+nohup /usr/local/share/desktop-init.sh &
